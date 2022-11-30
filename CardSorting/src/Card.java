798fcc0beb083b1ad391aad2c58d8127e362cbe3
Marcoski05 <marco.crivas24@bishopireton.org>
2022-11-30 14:49:46 -0500

    Fields, Constructors, and Getters

--- CardSorting/src/Card.java
+++ CardSorting/src/Card.java
@@ -1,4 +1,110 @@
 
-public class Card {
+public class Card implements Comparable<Card> {
 
+	private enum Suits {
+		HEARTS, DIAMONDS, CLUBS, SPADES
+	}
+
+	private Suits suit;
+	private int rank;
+
+	/**
+	 * 
+	 */
+	public Card() {
+		suit = Suits.SPADES;
+		rank = 1;
+	}
+
+	/**
+	 * @param suit
+	 * @param rank
+	 */
+	public Card(int suit, int rank) {
+		super();
+		this.suit = Suits.values()[suit];
+		this.rank = rank;
+	}
+
+	/**
+	 * @param suit
+	 * @param rank
+	 */
+	public Card(String suit, String rank) {
+
+		this.suit = Suits.valueOf(suit);
+		switch (rank.toUpperCase()) {
+		case "ACE":
+			this.rank = 1;
+			break;
+		case "JACK":
+			this.rank = 11;
+			break;
+		case "QUEEN":
+			this.rank = 12;
+			break;
+		case "KING":
+			this.rank = 13;
+			break;
+		default:
+			this.rank = Integer.parseInt(rank);
+			break;
+		}
+	}
+
+	/**
+	 * @param suit
+	 * @param rank
+	 */
+	public Card(String suit, int rank) {
+		this.suit = Suits.valueOf(suit);
+		this.rank = rank;
+	}
+
+	/**
+	 * @param suit
+	 * @param rank
+	 */
+	public Card(int suit, String rank) {
+		this.suit = Suits.values()[suit];
+		switch (rank.toUpperCase()) {
+		case "ACE":
+			this.rank = 1;
+			break;
+		case "JACK":
+			this.rank = 11;
+			break;
+		case "QUEEN":
+			this.rank = 12;
+			break;
+		case "KING":
+			this.rank = 13;
+			break;
+		default:
+			this.rank = Integer.parseInt(rank);
+			break;
+		}
+	}
+
+	/**
+	 * @param c
+	 */
+	public Card(Card c) {
+		suit = c.getSuit();
+		rank = c.getRank();
+	}
+	
+	/**
+	 * @return the suit
+	 */
+	public Suits getSuit() {
+		return suit;
+	}
+
+	/**
+	 * @return the rank
+	 */
+	public int getRank() {
+		return rank;
+	}
 }
